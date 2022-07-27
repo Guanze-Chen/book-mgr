6020d3bf5301bb6ad3f2b4e82d904cee8f6b051e
Guanze <ScuGuanze@163.com>
2022-07-28 04:50:17 +0800

    Login

---
+++ front-end/src/views/Auth/index.js
@@ -0,0 +1,18 @@
+import { defineComponent } from "vue";
+
+import {
+    UserOutlined,
+    KeyOutlined,
+    GiftOutlined,
+  } from '@ant-design/icons-vue';
+
+export default defineComponent({
+    components: {
+        UserOutlined,
+        KeyOutlined,
+        GiftOutlined,
+    },
+    setup() {
+
+    }
+})
---
+++ front-end/src/views/Auth/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="auth">
+    <div class="title-info">
+      <img
+        src="https://www.scu.edu.cn/__local/B/67/25/DFAF986CCD6529E52D7830F180D_C37C7DEE_4340.png"
+        alt="logo"
+      />
+      <h2 class="title">图书管理系统 后台</h2>
+    </div>
+
+    <div class="taps">
+      <a-tabs>
+        <a-tab-pane key="1" tab="登录">
+        <div class="item">
+            <a-input placeholder="账号">
+              <template #prefix>
+                <user-outlined />
+              </template>
+            </a-input>
+          </div>
+          <div class="item">
+            <a-input placeholder="密码">
+                <template #prefix>
+                     <key-outlined />
+                </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a href="#">忘记密码</a>
+          </div>
+
+          <div class="item">
+            <a-button type="primary">登录</a-button>
+          </div>
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="注册">
+          <div class="item">
+            <a-input placeholder="账号">
+              <template #prefix>
+                <user-outlined />
+              </template>
+            </a-input>
+          </div>
+          <div class="item">
+            <a-input placeholder="密码">
+                <template #prefix>
+                     <key-outlined />
+                </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a-input placeholder="邀请码">
+                <template #prefix>
+                   <gift-outlined />
+                </template>
+            </a-input>
+          </div>
+
+          <div class="item">
+            <a href="#">忘记密码</a>
+          </div>
+
+          <div class="item">
+            <a-button type="primary">注册</a-button>
+          </div>
+        </a-tab-pane>
+      </a-tabs>
+    </div>
+  </div>
+</template>
+
+
+<script src="./index.js">
+</script>
+
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
